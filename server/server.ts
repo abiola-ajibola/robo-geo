755ddf1373b7658a9180e2ab8574a2976bc5327b
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2022-09-07 15:39:09 +0100

    Request tile layers through server

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as dotenv from "dotenv";
 import express, { Request, Response, Express } from "express";
 import path from "node:path";
@@ -6,12 +7,28 @@ const app: Express = express();
 
 const { cwd, env } = process;
 
-const { NODE_PORT } = env;
+const { NODE_PORT, TILELAYER_BASE_URL } = env;
 
 const buildPath = path.resolve(cwd(), "build");
 app.use(express.static(buildPath));
 
-app.get("/*", (req: Request, res: Response) => {
+app.get("/gettiles", async (req: Request, res: Response) => {
+  const { query } = req;
+  const s = query.s;
+  const { x, y, z } = query;
+  const url = s
+    ? `https://${s}.${TILELAYER_BASE_URL}/${z}/${x}/${y}`
+    : `https://${TILELAYER_BASE_URL}/${z}/${x}/${y}`;
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    res.send(response.data as Buffer);
+  } catch (e: any) {
+    console.log({ e: e.message });
+  }
+});
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("*");
   res.sendFile(path.join(buildPath, "index.html"), (err) => {
     if (err) console.error(err);
   });
